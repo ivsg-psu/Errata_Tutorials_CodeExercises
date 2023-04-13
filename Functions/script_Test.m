5e079cea257ba3cd6cfe4f990c3513ad254cc8a2
aneeshbatchu <abb6486@psu.edu>
2023-04-12 21:30:55 -0400

    FEAT: This .m file is created to debug the errors in CoordX functions. This will be deleted once the gradeCoordX is created.

---
+++ Functions/script_Test.m
@@ -0,0 +1,18 @@
+
+clc
+%% First Assignment
+
+%help fcn_CoordX_01_Translation_KEEP
+
+%% Second Assignment
+
+%help fcn_CoordX_02_Rotation_KEEP
+
+%% Third Assignment
+
+%help fcn_CoordX_03_practiceRegression_KEEP
+
+%% Fourth Assignment
+
+help fcn_CoordX_04_practiceScaling_KEEP
+
