1fd10e6182077f389c5e6da47eccccf7f2a326bc
aneeshbatchu <abb6486@psu.edu>
2023-04-12 21:46:34 -0400

    Update script_Test.m

--- Functions/script_Test.m
+++ Functions/script_Test.m
@@ -2,15 +2,15 @@
 clc
 %% First Assignment
 
-%help fcn_CoordX_01_Translation_KEEP
+help fcn_CoordX_01_Translation_KEEP
 
 %% Second Assignment
 
-%help fcn_CoordX_02_Rotation_KEEP
+help fcn_CoordX_02_Rotation_KEEP
 
 %% Third Assignment
 
-%help fcn_CoordX_03_practiceRegression_KEEP
+help fcn_CoordX_03_practiceRegression_KEEP
 
 %% Fourth Assignment
 
