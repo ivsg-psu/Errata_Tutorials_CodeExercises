ba56b351984b719ff66b0bf30b5a320bbb604496
aneeshbatchu <abb6486@psu.edu>
2023-04-14 16:50:44 -0400

    FEAT: Instructions are updated

--- Functions/CoordX_Functions/fcn_CoordX_01_Translation_KEEP.m
+++ Functions/CoordX_Functions/fcn_CoordX_01_Translation_KEEP.m
@@ -3,58 +3,53 @@ function [random_points, translation_vector] = fcn_CoordX_01_Translation(varargi
 % the transformation
 %   
 %   Welcome to FCN_COORDX_01_TRANSLATION. In this assignment, you are given
-%   some set of random points and a translation_vector. The user is
-%   supposed to translate the random points using the translation vector
-%   and find the closest point to the orgin after translating the points.
-%
-%   The answer to this excercise is the row_index of the closest point to
-%   the origin after the translation has applied.
+%   some random points and a translation_vector. The user is supposed to
+%   translate the random points using the translation vector and find the
+%   closest point to the origin after translating the points. The answer to
+%   this exercise is the row_index of the closest point to the origin after
+%   applying the translation.
 %   
-%
 %   [random_points, translation_vector] =
 %   fcn_CoordX_01_Translation(entry_key,student_number);
 %   
-%   How to translate the points? 
+%   How to translate the points?
 %   
 %   You have to create the transformation matrix.
 %    
-%   (tx, ty) = translation_vector
-%
+%   (tx, ty) = translation_vector  - Given 
+%   
 %   transformation_matrix = [1 0 tx; 0 1 ty; 0 0 1]
 %
-%   For translating the points, 
-%   use -- (transformation_matrix*homogenous_random_points')'
+%   For translating the points, use --
+%   (transformation_matrix*homogenous_random_points')'
 %   
-%   " ' "  --  Transpose of a matrix
-%   Homogenous points: Z - co-ordinates of the randomly generated points
-%   should be '1'
-%
-%   To check if the answer is correct, call fcn_GradeCodeX. 
+%   " ' "  --  Transpose of a matrix Homogenous points: Z - coordinates of
+%   the randomly generated points should be '1' To check if the answer is
+%   correct, call fcn_GradeCoordX. 
+%   
 %   For example, to solve this, one could type in their script or at the
 %   MATLAB prompt:
-% 
-%            entry_key = ' 234ADH&45'; % <--- this must be changed
-%            student_number = 1234; % <--- this must be changed 
-%            row_index =
-%            fcn_CoordX_01_Translation(entry_key,student_number);
 %
-%            (write code or inspect the list here to find which row has the
+%            entry_key = ' 234ADH&45'; % <--- this must be changed
+%            student_number = 1234; % <--- this must be changed row_index =
+%            fcn_CoordX_01_Translation(entry_key,student_number); (write
+%            code or inspect the list here to find which row has the
 %            answer)
 %            
 %            answer_01_closestPoint_Index = row_index;
 %
-%            fcn_GradeCodeX('fcn_CoordX_01_Translation',answer_01_closestPoint_Index);
+%            fcn_GradeCoordX('fcn_CoordX_01_Translation',answer_01_closestPoint_Index);
 %
-%
-%   Or, to skip entry of the student number, the student number can be
+%   Or, to skip the entry of the student number, the student number can be
 %   entered via code before calling the grading function:
 %
 %            student_number = 1234; % <--- this must be changed
-%            fcn_GradeCodeX('fcn_CoordX_01_Translation',answer_01_closestPoint_Index,
+%
+%            fcn_GradeCoordX('fcn_CoordX_01_Translation',answer_01_closestPoint_Index,
 %            student_number);
 %
-%   If the answer is right, the grader prints information and gives you the next
-%   problem. 
+%   If the answer is correct, the grader prints the information and gives
+%   you the next problem.
 
 %   What does this code do? 
 %
@@ -65,10 +60,10 @@ function [random_points, translation_vector] = fcn_CoordX_01_Translation(varargi
 %   INPUTS:
 %
 %        entry_key: the entry key for the function, given by the
-%        fcn_GradeCodeX operations
+%        fcn_GradeCoordX operations
 %
 %        student_number: the student number. It must match the prior
-%        numbers used in the fcn_GradeCodeX operations
+%        numbers used in the fcn_GradeCoordX operations
 %
 %   OUTPUTS:
 %
